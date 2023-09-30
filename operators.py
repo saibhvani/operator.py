d1ad25aadaa6e1e591f5765eb25a663e8da916e2
saibhvani <145922824+saibhvani@users.noreply.github.com>
2023-09-30 14:34:20 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,148 @@
+#airthematic operator
+#addition
+a = 10
+b = 20
+print( a + b )
+
+#subtraction
+a = 10
+b = 20
+print( a - b )
+
+#multiplication
+a = 10
+b = 20
+print( a * b )
+
+#division
+a = 10
+b = 20
+print( a / b )
+
+#modulo
+a = 10
+b = 20
+print( a % b )
+
+#floor division
+a = 10
+b = 20
+print( a // b)
+
+#assignment operator
+a = 10
+
+a = 10
+a = a + 2
+print(a)
+
+a = 10
+a = a - 2
+print(a)
+
+a = 10
+a = a * 2
+print(a)
+
+a = 10
+a = a % 2
+print(a)
+
+a = 10
+a = a / 2
+print(a)
+
+a = 10
+a = a // b
+print(a)
+
+#logical operation
+#logical and
+a = 12
+b = 15
+c = 13
+print(a<b and a>c)
+
+#logical or
+a = 12
+b = 15
+c = 13
+print(a<b or a>c)
+
+#logical not
+a = 12
+b = 15
+c = 13
+print(not(a<b and a>c))
+
+#bitwise operator
+a = 6
+b = 13
+print( a & b)
+
+a = 6
+b = 13
+print( a | b )
+
+a = 6
+b = 13
+print( a ^ b )
+
+#comparison operator
+a = 10
+b = 20
+print( a <= b )
+
+a = 10
+b = 20
+print( a >= b)
+
+a = 10
+b = 20
+print( a == b)
+
+a = 10
+b = 20
+print( a >> b)
+
+a = 10
+b = 20
+print( a << b )
+
+a = 10
+b = 20
+print( a != b)
+
+#left shift
+a = 14
+b = 16
+print( a >> b )
+
+#right shift
+a = 16
+b = 20
+print( a << b )
+
+#check address of a vaiable
+a = 10
+print(id(a))
+
+#is operator
+a = 10
+b = 10
+print( a is b )
+
+#isnot operator
+a = 10
+b = 10
+print( a is not b)
+
+#in operator
+lst = ["saibhavani" "sravani" "sravanthi"]
+name = "sravani"
+print(name in lst )
+
+#notin operator
+lst = ["saibhavani" "sravani" "sravanthi"]
+name = "chinni"
+print(name not in lst)
